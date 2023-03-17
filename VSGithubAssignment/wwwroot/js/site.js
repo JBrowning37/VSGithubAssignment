6f68c8684bca8e0a13d17becc8901adb7921ae66
JBrowning37 <122848265+JBrowning37@users.noreply.github.com>
2023-03-17 16:23:43 -0700

    Add project files.

---
+++ VSGithubAssignment/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+var string = "This is my favorite song at the moment:";
+var str = string.split("");
+var el = document.getElementById('str');
+(function animate() {
+    str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running);
+    var running = setTimeout(animate, 90);
+})();
